cee463b58b6daa27b2e3223da6bb0996f853871f
chandanakonchada30 <24331a05e2@mvgrce.edu.in>
2025-12-14 10:00:43 +0530

    Add files via upload

---
+++ week2-3.py
@@ -0,0 +1,9 @@
+#24331A05E2
+#TO PRINT ASCII VALUES OF CHARACTERS
+char1=input("Enter a character: ")
+print("ASCII value of", char1, "is", ord(char1))
+char2=input("Enter another character: ")
+print("ASCII value of", char2, "is", ord(char2))
+char3=input("Enter one more character: ")
+print("ASCII value of", char3, "is", ord(char3))
+
